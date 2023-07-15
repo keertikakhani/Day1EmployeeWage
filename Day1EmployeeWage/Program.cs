aff29d9800496573e627ab79a3cb0b31dfaf7071
Your Name <you@example.com>
2023-07-15 21:46:46 +0530

    Adding a welcome message

---
+++ Day1EmployeeWage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day1EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
